3e6ad934ad80be677c6958577554b45e067431c6
Doug Murphy <doug.murphy82@gmail.com>
2025-05-04 15:45:37 -0400

    Add console command to add employee to property.

---
+++ S1API/Property/PropertyType.cs
@@ -0,0 +1,37 @@
+﻿namespace S1API.Property
+{
+    /// <summary>
+    /// The types of properties in the game.
+    /// </summary>
+    public enum PropertyType
+    {
+        /// <summary>
+        /// The Barn property.
+        /// </summary>
+        Barn,
+        /// <summary>
+        /// The Bungalow property.
+        /// </summary>
+        Bungalow,
+        /// <summary>
+        /// The Docks property.
+        /// </summary>
+        DocksWarehouse,
+        /// <summary>
+        /// The Manor property.
+        /// </summary>
+        Manor,
+        /// <summary>
+        /// The Motel Room property.
+        /// </summary>
+        MotelRoom,
+        /// <summary>
+        /// The RV property.
+        /// </summary>
+        Rv,
+        /// <summary>
+        /// The Sweatshop property
+        /// </summary>
+        Sweatshop
+    }
+}
