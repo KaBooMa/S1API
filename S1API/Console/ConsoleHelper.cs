33b2cd5979bb71b2716532b717dcaf27e46a06e2
Doug Murphy <doug.murphy82@gmail.com>
2025-05-04 19:33:13 -0400

    Add console command to clear player's wanted level.

--- S1API/Console/ConsoleHelper.cs
+++ S1API/Console/ConsoleHelper.cs
@@ -115,6 +115,23 @@ namespace S1API.Console
         }
         
         /// <summary>
+        /// Clears the player's wanted level.
+        /// This method works across both IL2CPP and Mono builds.
+        /// </summary>
+        public static void ClearWanted()
+        {
+#if (IL2CPPMELON || IL2CPPBEPINEX)
+            var command = new ClearWanted();
+            var args = new Il2CppSystem.Collections.Generic.List<string>();
+#else
+            var command = new ClearWanted();
+            var args = new List<string>();
+#endif
+
+            command.Execute(args);
+        }
+        
+        /// <summary>
         /// Executes the ChangeCashCommand with the given amount.
         /// This method works across both IL2CPP and Mono builds.
         /// </summary>
