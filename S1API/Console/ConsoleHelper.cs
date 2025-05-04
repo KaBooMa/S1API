6f222ec86e71a7088c4babe0696fe0226dc031d6
Doug Murphy <doug.murphy82@gmail.com>
2025-05-04 15:45:37 -0400

    Add console command to add employee to property.

--- S1API/Console/ConsoleHelper.cs
+++ S1API/Console/ConsoleHelper.cs
@@ -1,10 +1,13 @@
 #if (IL2CPPMELON)
 using Il2CppSystem.Collections.Generic;
 using static Il2CppScheduleOne.Console;
+using Il2CppScheduleOne.Employees;
 #else
 using static ScheduleOne.Console;
 using System.Collections.Generic;
+using ScheduleOne.Employees;
 #endif
+using S1API.Property;
 
 namespace S1API.Console
 {
@@ -13,6 +16,27 @@ namespace S1API.Console
     /// </summary>
     public static class ConsoleHelper
     {
+        /// <summary>
+        /// Executes the AddEmployeeCommand with the given arguments.
+        /// This method works across both IL2CPP and Mono builds.
+        /// </summary>
+        /// <param name="employeeType">The type of employee.</param>
+        /// <param name="propertyName">The property to add the employee to. You must own the property.</param>
+        public static void AddEmployee(EEmployeeType employeeType, PropertyType propertyName)
+        {
+#if (IL2CPPMELON || IL2CPPBEPINEX)
+            var command = new AddEmployeeCommand();
+            var args = new Il2CppSystem.Collections.Generic.List<string>();
+#else
+            var command = new AddEmployeeCommand();
+            var args = new List<string>();
+#endif
+            args.Add(employeeType.ToString());
+            args.Add(propertyName.ToString());
+            
+            command.Execute(args);
+        }
+        
         /// <summary>
         /// Executes the ChangeCashCommand with the given amount.
         /// This method works across both IL2CPP and Mono builds.
