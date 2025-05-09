3e7941c0ff09a36de60cb44a9201c06573404559
Doug Murphy <doug.murphy82@gmail.com>
2025-05-09 19:27:42 -0400

    Use block-scoped namespace in VanillaVehicleList.cs

--- S1API/Vehicles/VanillaVehicleList.cs
+++ S1API/Vehicles/VanillaVehicleList.cs
@@ -1,32 +1,33 @@
-﻿namespace S1API.Vehicles;
-
-/// <summary>
-/// The vehicles available in vanilla.
-/// </summary>
-public enum VanillaVehicleList
+﻿namespace S1API.Vehicles
 {
     /// <summary>
-    /// The Bruiser vehicle.
+    /// The vehicles available in vanilla.
     /// </summary>
-    Bruiser,
-    /// <summary>
-    /// The Cheetah vehicle.
-    /// </summary>
-    Cheetah,
-    /// <summary>
-    /// The Dinkler vehicle.
-    /// </summary>
-    Dinkler,
-    /// <summary>
-    /// The Hounddog vehicle.
-    /// </summary>
-    Hounddog,
-    /// <summary>
-    /// The Shitbox vehicle.
-    /// </summary>
-    Shitbox,
-    /// <summary>
-    /// The Veeper vehicle.
-    /// </summary>
-    Veeper,
+    public enum VanillaVehicleList
+    {
+        /// <summary>
+        /// The Bruiser vehicle.
+        /// </summary>
+        Bruiser,
+        /// <summary>
+        /// The Cheetah vehicle.
+        /// </summary>
+        Cheetah,
+        /// <summary>
+        /// The Dinkler vehicle.
+        /// </summary>
+        Dinkler,
+        /// <summary>
+        /// The Hounddog vehicle.
+        /// </summary>
+        Hounddog,
+        /// <summary>
+        /// The Shitbox vehicle.
+        /// </summary>
+        Shitbox,
+        /// <summary>
+        /// The Veeper vehicle.
+        /// </summary>
+        Veeper,
+    }
 }
