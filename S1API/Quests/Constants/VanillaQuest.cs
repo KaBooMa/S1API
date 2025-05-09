d4fcb730bf362aec6ac5c292fda5c7d46bfd3450
Doug Murphy <doug.murphy82@gmail.com>
2025-05-09 19:23:53 -0400

    Add XML comments to VanillaQuest.cs

--- S1API/Quests/Constants/VanillaQuest.cs
+++ S1API/Quests/Constants/VanillaQuest.cs
@@ -2,44 +2,104 @@
 
 namespace S1API.Quests.Constants
 {
+    /// <summary>
+    /// The list of quests in vanilla.
+    /// </summary>
     public enum VanillaQuest
     {
+        /// <summary>
+        /// The Botanists quest
+        /// </summary>
         [Description("Botanists")]
         Botanists,
+        /// <summary>
+        /// The Chemists quest
+        /// </summary>
         [Description("Chemists")]
         Chemists,
+        /// <summary> 
+        /// The Clean Cash quest
+        /// </summary>
         [Description("Clean Cash")]
         Clean_Cash,
+        /// <summary>
+        /// The Cleaners quest
+        /// </summary>
         [Description("Cleaners")]
         Cleaners,
+        /// <summary>
+        /// The Dodgy Dealing quest
+        /// </summary>
         [Description("Dodgy Dealing")]
         Dodgy_Dealing,
+        /// <summary>
+        /// The Gearing Up quest
+        /// </summary>
         [Description("Gearing Up")]
         Gearing_Up,
+        /// <summary>
+        /// The Getting Started quest
+        /// </summary>
         [Description("Getting Started")]
         Getting_Started,
+        /// <summary>
+        /// The Keeping it Fresh quest
+        /// </summary>
         [Description("Keeping it Fresh")]
         Keeping_it_Fresh,
+        /// <summary>
+        /// The Making the Rounds quest
+        /// </summary>
         [Description("Making the Rounds")]
         Making_the_Rounds,
+        /// <summary>
+        /// The Mixing Mania quest
+        /// </summary>
         [Description("Mixing Mania")]
         Mixing_Mania,
+        /// <summary>
+        /// The Money Management quest
+        /// </summary>
         [Description("Money Management")]
         Money_Management,
+        /// <summary>
+        /// The Moving Up quest
+        /// </summary>
         [Description("Moving Up")]
         Moving_Up,
+        /// <summary>
+        /// The Needin' the Green quest
+        /// </summary>
         [Description("Needin' the Green")]
         NeedinTheGreen,
+        /// <summary>
+        /// The On the Grind quest
+        /// </summary>
         [Description("On the Grind")]
         On_the_Grind,
+        /// <summary>
+        /// The Packagers quest
+        /// </summary>
         [Description("Packagers")]
         Packagers,
+        /// <summary>
+        /// The Packin' quest
+        /// </summary>
         [Description("Packin'")]
         Packin,
+        /// <summary>
+        /// The We Need To Cook quest
+        /// </summary>
         [Description("We Need To Cook")]
         We_Need_To_Cook,
+        /// <summary>
+        /// The Welcome to Hyland Point quest
+        /// </summary>
         [Description("Welcome to Hyland Point")]
         Welcome_to_Hyland_Point,
+        /// <summary>
+        /// The Wretched Hive of Scum and Villainy quest
+        /// </summary>
         [Description("Wretched Hive of Scum and Villainy")]
         Wretched_Hive_of_Scum_and_Villainy,
     }
